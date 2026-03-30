b3f64553265910d65a0fb8a503b5de630187de83
Dylan-byt <dylan.johnson@city.ac.uk>
2026-03-30 17:29:45 +0000

    added files:     lib/sqlite-jdbc-
    i believe this was needed to accesss the sqlite database from java, and it was added to the project by downloading the jar file and adding it to the project structure. if this is not the case, i will delete the files and commit again.

---
+++ src/DBConnection.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:sqlite:SQL/CA_db.db";
+
+    public static Connection getConnection() {
+        try {
+            Connection conn = DriverManager.getConnection(URL);
+            System.out.println("Connected to database");
+            return conn;
+        } catch (SQLException e) {
+            System.out.println("Connection failed");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/DBTest.java
@@ -0,0 +1,26 @@
+import java.nio.file.Paths;
+import java.sql.*;
+
+public class DBTest {
+    public static void main(String[] args) {
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:/workspaces/IN-2033/SQL/CA_db.db");
+
+            System.out.println("Connected ✅");
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT name FROM sqlite_master WHERE type='table'");
+
+            System.out.println("Tables:");
+
+            while (rs.next()) {
+                System.out.println(rs.getString("name"));
+            }
+
+        } catch (Exception e) {
+            System.out.println("Connection failed ❌");
+            e.printStackTrace();
+        }
+    }
+}
