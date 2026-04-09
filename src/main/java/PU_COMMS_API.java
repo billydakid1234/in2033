5dd740aef689c6cc6b269ffe27c415be533ba5a0
reinh <Reinhardas.Girdziusas@city.ac.uk>
2026-04-09 05:32:52 +0100

    Updated PU_COMMS_API interface to accommodate processCardPayment method.

--- src/main/java/PU_COMMS_API.java
+++ src/main/java/PU_COMMS_API.java
@@ -1,14 +1,19 @@
 
-
-
 public interface PU_COMMS_API {
-    
-    /** 
-    * 
-    * @param recipient 
-    * @param subject 
-    * @param content 
-    */ 
-    abstract boolean sendEmail(String recipient, String subject, String content); 
 
-} 
+    /**
+     * Sends card payment details securely to PU subsystem.
+     * @param cardNumber Full card number (temporarily, must be encrypted in transit)
+     * @param expiry Expiry date (MM/YY)
+     * @param amount Payment amount
+     * @param orderID Associated order ID
+     *
+     * @param recipient
+     * @param subject
+     * @param content
+     */
+    boolean processCardPayment(String cardNumber, String expiry, double amount, String orderID);
+
+    abstract boolean sendEmail(String recipient, String subject, String content);
+
+}
